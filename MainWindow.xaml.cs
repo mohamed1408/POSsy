e69a2fae79e1877155ab705c9e02d7724a43442b
mohamed1408 <mohamedanastsi@gmail.com>
2022-03-29 08:40:50 +0530

    asdsdsad

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,5 +32,33 @@ namespace POS
                 DragMove();
             }
         }
+        private void min_btn(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void max_btn(object sender, RoutedEventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Normal:
+                    this.WindowState = WindowState.Maximized;
+                    break;
+                case WindowState.Maximized:
+                    this.WindowState = WindowState.Normal;
+                    break;
+            }
+        }
+        private void cls_btn(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Page_One(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+        private void Page_Two(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
     }
 }
---
+++ TheControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace POS
+{
+    static class TheControl
+    {
+        static bool isMax = false, isFull = false;
+        static Point old_loc, default_loc;
+        static Size old_size, default_size;
+
+        public static void SetInitials(Window win)
+        {
+            old_size = new Size(win.Width, win.Height);
+            old_loc = new Point(win.Top, win.Left);
+            default_size = new Size(win.Width, win.Height);
+            default_loc = new Point(win.Top, win.Left);
+        }
+
+        public static void FullScreen(Window win)
+        {
+            if (win.WindowState == WindowState.Normal)
+                win.WindowState = WindowState.Maximized;
+            else
+                win.WindowState = WindowState.Normal;
+        }
+
+    }
+}
